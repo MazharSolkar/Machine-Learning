eaa1d6799c03a7148c94ee2567ba21bb4be927e1
MazharSolkar <mazharsolkar65@gmail.com>
2022-07-26 18:07:36 +0530

    pract 1 modified

--- practical_01/practical_01.py
+++ practical_01/practical_01.py
@@ -2,27 +2,30 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
-df = pd.read_csv(r"C:\0_MSc_IT_Notes\Practicals\Machine Learning\practical_01\Cars.csv") #Read data
+df = pd.read_csv(r"C:\0_MSc_IT_Notes\Practicals\Machine Learning\practical_01\Cars.csv")
 print(df.head())
 
-plt.scatter(df['Milage'],df['Sell Price']) #Plot to compare Milage(Independant variable) with Sell Price(Dependant variable)
+#Plot to compare Milage(Independant variable) with Sell Price(Dependant variable)
+plt.scatter(df['Milage'],df['Sell Price'])
 plt.show()
-plt.scatter(df['Age'],df['Sell Price'])#Plot to compare Age(Independant variable) with Sell Price(Dependant variable)
+#Plot to compare Age(Independant variable) with Sell Price(Dependant variable)
+plt.scatter(df['Age'],df['Sell Price'])
 plt.show()
 X = df[['Milage','Age']] #Determine X
-Y = df['Sell Price']    # Determine Y
+Y = df['Sell Price']     #Determine Y
 
 print("\nindependent varaible x :- \n",X) #Display X
 print("\ndependent variable y :- \n",Y) #Display Y
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.2)#Create a training set and testing set
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.2)
 print("\nlength of x_train = ",len(X_train))
 print("\nlength of X_test =",len(X_test))
 
 from sklearn.linear_model import LinearRegression
 clf = LinearRegression()
 clf.fit(X_train,Y_train) # Train the model
-print("\npredicted X_test :- \n",clf.predict(X_test)) # Use the trained model to predict the testing set
+print("\npredicted X_test :- \n",clf.predict(X_test))
 
 print("\naccuracy = ",clf.score(X_test,Y_test)) #Calculate the accuracy
+
